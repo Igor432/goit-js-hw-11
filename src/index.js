438bc7525ab3402843ce953b6dadc7f2e6388f21
Igor432 <igomar0602@gmail.com>
2022-07-22 17:15:00 +0300

    server

--- src/index.js
+++ src/index.js
@@ -4,48 +4,46 @@ const input = document.querySelector('input');
 const gallery = document.querySelector('.gallery')
 const form = document.querySelector('.search-form');
 
-const photoList = [];
+
 
 
 async function getPhoto(name) {
 
   const photos = await axios.get(`https://pixabay.com/api/?key=28780636-ee20ed417c8a5aa1eeee48e35&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`);
-  photoList.push(photos.data.hits);
-  console.log(photoList)
+  const photoList = photos.data.hits;
 
-}
-
-
-function makeList() {
-  photoList.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-    `<div class="photo-card">
-  <img src=${webformatURL} alt="" loading="lazy" />
+  const galleryList = photoList.map((photo) => {
+    return `<div class="photo-card">
+  <img src=${photo.webformatURL} alt="" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>${likes}Likes</b>
+      <b>${photo.likes}Likes</b>
     </p>
     <p class="info-item">
-      <b>${views}Views</b>
+      <b>${photo.views}Views</b>
     </p>
     <p class="info-item">
-      <b>${comments}Comments</b>
+      <b>${photo.comments}Comments</b>
     </p>
     <p class="info-item">
-      <b>${downloads}Downloads</b>
+      <b>${photo.downloads}Downloads</b>
     </p>
   </div>
 </div>`
-    gallery.innerHTML = photoList;
-
 
   })
+  gallery.innerHTML = galleryList;
+
 }
+
 
 
 
 form.addEventListener('submit', (evt) => {
   var inputText = input.value;
-  getPhoto(inputText).then(photoList => makeList(photoList))
+  getPhoto(inputText).then()
+
+
   evt.preventDefault();
   console.log('search done')
 })
@@ -58,3 +56,11 @@ form.addEventListener('submit', (evt) => {
   { webformatURL, largeImageURL, tags, likes, views, comments, downloads }
 
   */
+
+
+
+
+
+
+
+
