2573cc875a710f57b8655959520e96e9c6705ce6
Igor432 <igomar0602@gmail.com>
2022-07-23 16:32:11 +0300

    addPhotos

--- src/index.js
+++ src/index.js
@@ -5,17 +5,27 @@ const gallery = document.querySelector('.gallery')
 const form = document.querySelector('.search-form');
 
 document.querySelector('.load-more').style.display = 'none';
+const loadMore = document.querySelector('.load-more');
 
+
+let page = 1;
+let perPage = 40;
 
 
 async function getPhoto(name) {
 
-  const photos = await axios.get(`https://pixabay.com/api/?key=28780636-ee20ed417c8a5aa1eeee48e35&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`);
-  const photoList = photos.data.hits;
+  const photos = await axios.get(`https://pixabay.com/api/?key=28780636-ee20ed417c8a5aa1eeee48e35&q=${name}&image_type=photo&orientation=horizontal&safesearch=true$&per_page=${perPage}&page=${page}`);
+  return photos.data.hits;
 
-  const galleryList = photoList.map((photo) => {
+}
+
+
+
+
+function makeList(photos) {
+  const galleryList = photos.map((photo) => {
     return `<div class="photo-card">
-  <img src=${photo.webformatURL} alt="" loading="lazy" />
+  <img src=${photo.webformatURL} alt="" loading="lazy" width='500px' heigh='auto' />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -37,21 +47,42 @@ async function getPhoto(name) {
 </div>`
 
   })
-  gallery.innerHTML = galleryList;
+  gallery.insertAdjacentHTML("beforeend", galleryList);;
 
 }
+
+
+loadMore.addEventListener('click', () => {
+  var inputText = input.value;
+  page += 1;
+  perPage += 40;
+  getPhoto(inputText).then((photos) => {
+
+    makeList(photos)
+
+  })
+})
 
 
 
 
 form.addEventListener('submit', (evt) => {
   var inputText = input.value;
-  getPhoto(inputText).then()
+  getPhoto(inputText).then(photos => makeList(photos))
 
   document.querySelector('.load-more').style.display = 'block';
   evt.preventDefault();
   console.log('search done')
+
+
+
+
+
+
 })
+
+
+
 
 
 
